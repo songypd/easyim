2fc0263bab03cccf4f0afcea387a8a94ee0425d0
songyuanpeng <songyuanpeng@aixuexi.com>
2021-11-29 22:11:00 +0800

    feat>javaTest

---
+++ src/test/java/com/easy/im/easyim/BasicJavaLearnTest.java
@@ -0,0 +1,19 @@
+package com.easy.im.easyim;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author yuanpeng.song
+ * @createTime 2021/11/29
+ * @description java核心编程test
+ * Laughter is poison to fear.
+ */
+public class BasicJavaLearnTest {
+    @Test
+    public void testStr(){
+        //返回一个字符串，用给定的定界符，连接所有符号
+        String join = String.join("_", "hello", "world", "my", "name", "is", "songypd");
+//        System.out.println(join);
+
+    }
+}
